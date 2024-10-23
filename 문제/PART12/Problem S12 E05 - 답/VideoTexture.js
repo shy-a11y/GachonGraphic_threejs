bf83534ef1cb386ae558292764885c6473744508
YEONGBIN PAK <suplem07@naver.com>
2024-10-23 12:14:31 +0900

    PART 12 문제 추가

---
+++ 문제/PART12/Problem S12 E05 - 답/VideoTexture.js
@@ -0,0 +1,52 @@
+class VideoTexture extends THREE.Texture {
+
+	constructor( video, mapping, wrapS, wrapT, magFilter, minFilter, format, type, anisotropy ) {
+
+		super( video, mapping, wrapS, wrapT, magFilter, minFilter, format, type, anisotropy );
+
+		this.format = format !== undefined ? format : THREE.RGBFormat;
+
+		this.minFilter = minFilter !== undefined ? minFilter : THREE.LinearFilter;
+		this.magFilter = magFilter !== undefined ? magFilter : THREE.LinearFilter;
+
+		this.generateMipmaps = false;
+
+		const scope = this;
+
+		function updateVideo() {
+
+			scope.needsUpdate = true;
+			video.requestVideoFrameCallback( updateVideo );
+
+		}
+
+		if ( 'requestVideoFrameCallback' in video ) {
+
+			video.requestVideoFrameCallback( updateVideo );
+
+		}
+
+	}
+
+	clone() {
+
+		return new this.constructor( this.image ).copy( this );
+
+	}
+
+	update() {
+
+		const video = this.image;
+		const hasVideoFrameCallback = 'requestVideoFrameCallback' in video;
+
+		if ( hasVideoFrameCallback === false && video.readyState >= video.HAVE_CURRENT_DATA ) {
+
+			this.needsUpdate = true;
+
+		}
+
+	}
+
+}
+
+VideoTexture.prototype.isVideoTexture = true;
